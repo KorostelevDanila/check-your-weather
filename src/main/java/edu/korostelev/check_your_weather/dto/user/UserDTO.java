2a1dfaa26941e3940f9a17bb57de95313393845f
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-02 18:05:59 +0300

    create UserDTO

---
+++ src/main/java/edu/korostelev/check_your_weather/dto/user/UserDTO.java
@@ -0,0 +1,11 @@
+package edu.korostelev.check_your_weather.dto.user;
+
+public class UserDTO {
+    private Integer id;
+    private String login;
+
+    public UserDTO(Integer id, String login) {
+        this.id = id;
+        this.login = login;
+    }
+}
