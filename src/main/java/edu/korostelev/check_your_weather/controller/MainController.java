aec0831eead57e0965ab7760692bf75bbf91ea17
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-02 18:07:31 +0300

    add security profile and model to main controller

--- src/main/java/edu/korostelev/check_your_weather/controller/MainController.java
+++ src/main/java/edu/korostelev/check_your_weather/controller/MainController.java
@@ -1,6 +1,9 @@
 package edu.korostelev.check_your_weather.controller;
 
+import edu.korostelev.check_your_weather.security.SecurityProfile;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,7 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class MainController {
     @GetMapping
-    public String mainPage() {
+    public String mainPage(@AuthenticationPrincipal SecurityProfile profile,
+                           Model model) {
+        model.addAttribute("user", profile);
         return "index";
     }
 }
