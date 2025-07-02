6a630d0e9a980c5f48f6581bae228160cc64b0bf
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-07-02 21:31:52 +0300

    add password encoding on user registration

--- src/main/java/edu/korostelev/check_your_weather/service/DatabaseRegistrationService.java
+++ src/main/java/edu/korostelev/check_your_weather/service/DatabaseRegistrationService.java
@@ -4,19 +4,22 @@ import edu.korostelev.check_your_weather.dto.user.SignUpRequestCredentials;
 import edu.korostelev.check_your_weather.dto.user.UserDTO;
 import edu.korostelev.check_your_weather.model.User;
 import edu.korostelev.check_your_weather.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DatabaseRegistrationService implements RegistrationService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public DatabaseRegistrationService(UserRepository userRepository) {
+    public DatabaseRegistrationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public UserDTO register(SignUpRequestCredentials credentials) {
-        User recievedUser = new User(credentials.getLogin(), credentials.getPassword());
+        User recievedUser = new User(credentials.getLogin(), passwordEncoder.encode(credentials.getPassword()));
         User createdUser = userRepository.save(recievedUser);
         UserDTO userDTO = new UserDTO(createdUser.getId(), createdUser.getLogin());
         return userDTO;
